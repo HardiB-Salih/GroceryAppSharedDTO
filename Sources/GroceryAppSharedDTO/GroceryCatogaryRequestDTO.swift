15ae35da9f38cbdb3da83c16bdc639b8e20172b6
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2024-05-25 18:49:39 +0300

    added DTO for grocary catogery

--- Sources/GroceryAppSharedDTO/GroceryCatogaryRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCatogaryRequestDTO.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+public struct GroceryCatogaryRequestDTO: Codable {
+    
+    public let title: String
+    public let colorCode: String
+    
+    public init(title: String, colorCode: String) {
+        self.title = title
+        self.colorCode = colorCode
+    }
+    
+    
+}
